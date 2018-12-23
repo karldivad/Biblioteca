4fb967c66f1d04d8ad814a75fda920df5bcf9227
karldivad <carlosvalenciarivero@gmail.com>
2018-12-23 05:29:26 -0500

    allowed hosts fix

--- library/settings/__init__.py
+++ library/settings/__init__.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'i2$9dspgl*(s-*y6ea*)i=)ia%3=r9630n_quumlva#6u1e8xt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["librarycs.herokuapp.com"]
+ALLOWED_HOSTS = ["librarycs.herokuapp.com","localhost"]
 
 
 # Application definition
